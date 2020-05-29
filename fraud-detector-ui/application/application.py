b3ec5735d6bfce799eec66884beb5311789a2574
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2020-05-29 20:14:34 +0300

    add clients for kafka, redis, and cassandra

--- fraud-detector-ui/application/application.py
+++ fraud-detector-ui/application/application.py
@@ -1,34 +1,57 @@
 import random
+from datetime import datetime
+from typing import List
 
+from cassandra.cluster import Cluster
 from flask import Flask, render_template, request, jsonify
+from kafka import KafkaProducer, KafkaConsumer
+from redis import Redis
+
+from application.clients import Clients
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def get_index():
     return render_template('index.html')
+
+
+@app.route('/status')
+def get_status():
+    consumer: KafkaConsumer = Clients.get('kafka-consumer')
+    kafka_topics = consumer.topics()
+    print(f'topics: {kafka_topics}')
+    kafka_status = 'events' in kafka_topics
+    redis_client: Redis = Clients.get('redis')
+    redis_info = redis_client.info()
+    print(f'redis_info: {redis_info}')
+    redis_status = bool(redis_info)
+    return jsonify(kafka_status=kafka_status, redis_status=redis_status)
 
 
 @app.route('/events', methods=['POST'])
 def create_events():
-    ips = request.form['ips'].split(',')
+    ips: List[str] = request.form['ips'].split(',')
+    producer: KafkaProducer = Clients.get('kafka-producer')
+    now = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
     for ip in ips:
-        print(ip)
+        producer.send('events', key=ip.encode('utf-8'), value=now.encode('utf-8'))
     return '', 201
 
 
 @app.route('/bots')
 def get_bots():
-    ips = [
-        '1.2.3.4', '5.6.7.8', '9.10.11.12', '13.14.15.16',
-        '17.18.19.20', '21.22.23.24', '25.26.27.28',
-    ]
-    all_bots = random.sample(ips, random.randint(0, len(ips) - 1))
-    return jsonify(bots=all_bots)
+    redis_client: Redis = Clients.get('redis')
+    bots = [bot for bot in redis_client.scan_iter('bots:*')]
+    return jsonify(bots=bots)
 
 
 @app.route('/total')
 def get_total():
+    cassandra_cluster: Cluster = Clients.get('cassandra')
+    session = cassandra_cluster.connect('system')
+    res = session.execute('SELECT * FROM schema_keyspaces;')
+    print(res)
     total = random.randint(0, 10)
     return jsonify(total=total)
---
+++ fraud-detector-ui/application/clients.py
@@ -0,0 +1,48 @@
+import os
+
+from cassandra.cluster import Cluster
+from kafka import KafkaProducer, KafkaConsumer
+from redis import Redis
+
+
+class Clients:
+    clients = {}
+
+    @classmethod
+    def get(cls, client_name):
+        client = cls.clients.get(client_name, None)
+        if client:
+            return client
+        if client_name == 'redis':
+            client = cls._create_redis()
+        elif client_name == 'kafka-producer':
+            client = cls._create_kafka_producer()
+        elif client_name == 'kafka-consumer':
+            client = cls._create_kafka_consumer()
+        elif client_name == 'cassandra':
+            client = cls._create_cassandra()
+        else:
+            raise RuntimeError(f'Unknown client name: {client_name}')
+        cls.clients[client_name] = client
+        return client
+
+    @staticmethod
+    def _create_redis():
+        host, port = os.environ['REDIS'].split(':')
+        return Redis(host=host, port=int(port), db=0)
+
+    @staticmethod
+    def _create_kafka_producer():
+        bootstrap_servers = os.environ['KAFKA']
+        return KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+    @staticmethod
+    def _create_kafka_consumer():
+        bootstrap_servers = os.environ['KAFKA']
+        return KafkaConsumer(bootstrap_servers=bootstrap_servers)
+
+    @staticmethod
+    def _create_cassandra():
+        host = os.environ['CASSANDRA'].split(':')[0]
+        cluster = Cluster([host])
+        return cluster
