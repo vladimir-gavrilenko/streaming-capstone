23ee04c6b3aa99966a55abec79f7d0b801868797
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2020-05-29 16:32:19 +0300

    implement receiving active bots

--- fraud-detector-ui/application/application.py
+++ fraud-detector-ui/application/application.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import random
+
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,6 +12,17 @@ def index():
 
 @app.route('/events', methods=['POST'])
 def event():
-    ip = request.form['ip']
-    print(ip)
+    ips = request.form['ips'].split(',')
+    for ip in ips:
+        print(ip)
     return '', 201
+
+
+@app.route('/bots')
+def bots():
+    ips = [
+        '1.2.3.4', '5.6.7.8', '9.10.11.12', '13.14.15.16',
+        '17.18.19.20', '21.22.23.24', '25.26.27.28',
+    ]
+    all_bots = random.sample(ips, random.randint(0, len(ips) - 1))
+    return jsonify(bots=all_bots)
--- fraud-detector-ui/application/static/js/script.js
+++ fraud-detector-ui/application/static/js/script.js
@@ -1,13 +1,35 @@
-function submitEvent() {
+function submitEvents() {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/events', true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-    if (this.readyState === XMLHttpRequest.DONE) {
-            console.log('event submitting status: ' + this.status);
-        }
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        console.log('event submitting status: ' + xhr.status);
     }
-    ip = document.getElementById("ip").value;
-    console.log(ip)
-    xhr.send('ip=' + ip);
+    ips = document.getElementById('ips').value;
+    console.log(ips);
+    xhr.send('ips=' + ips);
+}
+
+function displayBots() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/bots', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        var bots = JSON.parse(xhr.responseText)['bots'];
+        console.log(bots);
+        var botsElement = document.getElementById('active-bots');
+        botsElement.innerHTML = ''
+        for (var i = 0; i < bots.length; i++) {
+            var botElement = document.createElement('li');
+            var botElementText = document.createTextNode(bots[i]);
+            botElement.appendChild(botElementText);
+            botsElement.append(botElement);
+        }
+    };
+    xhr.send(null);
+}
+
+function main() {
+    console.log('main')
+    setInterval(displayBots, 2000);
 }
