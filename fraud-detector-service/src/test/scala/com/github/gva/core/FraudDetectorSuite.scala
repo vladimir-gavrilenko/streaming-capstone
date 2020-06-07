3f5aa90cec0bd82b0801610e76407e91fc2c4630
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2020-06-07 23:54:38 +0300

    start adding unit tests

---
+++ fraud-detector-service/src/test/scala/com/github/gva/core/FraudDetectorSuite.scala
@@ -0,0 +1,18 @@
+package com.github.gva.core
+
+import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.scalatest.FunSuite
+
+class FraudDetectorSuite extends FunSuite with DataFrameSuiteBase {
+  test("regular ip addresses are not marked as bots") {
+
+  }
+
+  test("suspicious ip addresses are marked as bots") {
+
+  }
+
+  test("suspicious ip addresses can be removed from the bot list after some time") {
+
+  }
+}
