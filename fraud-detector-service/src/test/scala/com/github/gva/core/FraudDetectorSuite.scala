420081fb5a7b22e10d582d1dec86dd00a99ff279
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2020-06-08 00:38:05 +0300

    add basic unit test

--- fraud-detector-service/src/test/scala/com/github/gva/core/FraudDetectorSuite.scala
+++ fraud-detector-service/src/test/scala/com/github/gva/core/FraudDetectorSuite.scala
@@ -1,18 +1,55 @@
 package com.github.gva.core
 
+import java.util.UUID.randomUUID
+
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.execution.streaming.MemoryStream
+import org.apache.spark.sql.streaming.OutputMode
 import org.scalatest.FunSuite
 
 class FraudDetectorSuite extends FunSuite with DataFrameSuiteBase {
   test("regular ip addresses are not marked as bots") {
-
+    val events = (1 to 100)
+      .flatMap(s => Seq(
+        Event(ip = "1.2.3.4", action = "click", epochSeconds = s),
+        Event(ip = "5.6.7.8", action = "click", epochSeconds = s + 1)
+      ))
+    val bots = process(events)
+    assert(bots.isEmpty)
   }
 
   test("suspicious ip addresses are marked as bots") {
-
+    val events = (1 to 100)
+      .flatMap(s => Seq(
+        Event(ip = "1.2.3.4", action = "click", epochSeconds = s),
+        Event(ip = "1.2.3.4", action = "click", epochSeconds = s + 1),
+        Event(ip = "1.2.3.4", action = "click", epochSeconds = s + 2),
+        Event(ip = "5.6.7.8", action = "click", epochSeconds = s)
+      ))
+    val bots = process(events)
+    bots.show()
   }
 
   test("suspicious ip addresses can be removed from the bot list after some time") {
 
+  }
+
+  def process(eventsData: Seq[Event]): DataFrame = {
+    import spark.implicits._
+    val queryName = s"sink_${randomUUID()}".replace("-", "_")
+    val fraudDetector = new FraudDetector
+    val eventsSource = MemoryStream[Event]
+    val events = eventsSource.toDF()
+    val bots = fraudDetector.detectBots(events)
+    val outputQuery = bots
+      .writeStream
+      .format("memory")
+      .queryName(queryName)
+      .outputMode(OutputMode.Append)
+      .start()
+    eventsSource.addData(eventsData)
+    outputQuery.processAllAvailable()
+    spark.table(queryName)
   }
 }
