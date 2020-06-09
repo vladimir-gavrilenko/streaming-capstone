d4c191d0721410694d09bb459f862cbcc5bdefec
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2020-06-09 19:35:31 +0300

    implement bot detection logic

--- fraud-detector-service/src/main/scala/com/github/gva/core/FraudDetector.scala
+++ fraud-detector-service/src/main/scala/com/github/gva/core/FraudDetector.scala
@@ -1,9 +1,30 @@
 package com.github.gva.core
 
+import org.apache.spark.sql.functions._
+
 import org.apache.spark.sql.DataFrame
 
+/**
+ * Bot detection algorithm:
+ *  - More than 20 requests per 10 seconds for single IP.
+ *  - IP should be whitelisted in 10 minutes if bot detection condition is not matched.
+ *    It means IP should be deleted from Redis once host stops suspicious activity.
+ */
 class FraudDetector {
-  def detectBots(events: DataFrame): DataFrame = {
+  def detectBots(
+    events: DataFrame,
+    windowDuration: Int = 10,
+    slideDuration: Int = 1,
+    eventsPerWindowThreshold: Int = 20
+  ): DataFrame = {
     events
+      .withColumn("timestamp", to_timestamp(col("epochSeconds")))
+      .withWatermark("timestamp", s"$windowDuration seconds")
+      .groupBy(
+        col("ip"),
+        window(col("timestamp"), s"$windowDuration seconds", s"$slideDuration seconds")
+      )
+      .count()
+      .filter(col("count") > eventsPerWindowThreshold)
   }
 }
--- fraud-detector-service/src/test/scala/com/github/gva/core/FraudDetectorSuite.scala
+++ fraud-detector-service/src/test/scala/com/github/gva/core/FraudDetectorSuite.scala
@@ -6,6 +6,7 @@ import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.functions._
 import org.scalatest.FunSuite
 
 class FraudDetectorSuite extends FunSuite with DataFrameSuiteBase {
@@ -28,11 +29,27 @@ class FraudDetectorSuite extends FunSuite with DataFrameSuiteBase {
         Event(ip = "5.6.7.8", action = "click", epochSeconds = s)
       ))
     val bots = process(events)
-    bots.show()
+    assert(!bots.filter(col("ip") === "1.2.3.4").isEmpty)
+    assert(bots.filter(col("ip") === "5.6.7.8").isEmpty)
   }
 
   test("suspicious ip addresses can be removed from the bot list after some time") {
-
+    val oneBot = (1 to 100)
+      .flatMap(s => Seq(
+        Event(ip = "1.2.3.4", action = "click", epochSeconds = s),
+        Event(ip = "1.2.3.4", action = "click", epochSeconds = s + 1),
+        Event(ip = "1.2.3.4", action = "click", epochSeconds = s + 2),
+        Event(ip = "5.6.7.8", action = "click", epochSeconds = s)
+      ))
+    val noBots = (101 to 200)
+      .flatMap(s => Seq(
+        Event(ip = "1.2.3.4", action = "click", epochSeconds = s),
+        Event(ip = "5.6.7.8", action = "click", epochSeconds = s)
+      ))
+    val bots = process(oneBot ++ noBots)
+    val botStoppedTime = to_timestamp(lit(101))
+    assert(!bots.filter(col("window.start") < botStoppedTime).isEmpty)
+    assert(bots.filter(col("window.start") > botStoppedTime).isEmpty)
   }
 
   def process(eventsData: Seq[Event]): DataFrame = {
